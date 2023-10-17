ccf11e53b854da041c6ed77dcd4d76e7c56f4273
unknown <toolvlg@gmail.com>
2023-10-17 15:47:33 +0400

    deploy fix 5

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,21 +1,41 @@
-export const getUserQuery = `
-    query GetUser($email: String!) {
-        user(by: { email: $email }) {
-            id
-            name
-            email
-            avatarUrl
-            projects(last: 4) {
-              edges {
-                node {
-                  id
-                  title
-                  image
-                }
-              }
-            }
-        }
+export const createProjectMutation = `
+	mutation CreateProject($input: ProjectCreateInput!) {
+		projectCreate(input: $input) {
+			project {
+				id
+				title
+				description
+				createdBy {
+					email
+					name
+				}
+			}
+		}
+	}
+`;
+
+export const updateProjectMutation = `
+	mutation UpdateProject($id: ID!, $input: ProjectUpdateInput!) {
+		projectUpdate(by: { id: $id }, input: $input) {
+			project {
+				id
+				title
+				description
+				createdBy {
+					email
+					name
+				}
+			}
+		}
+	}
+`;
+
+export const deleteProjectMutation = `
+  mutation DeleteProject($id: ID!) {
+    projectDelete(by: { id: $id }) {
+      deletedId
     }
+  }
 `;
 
 export const createUserMutation = `
@@ -25,80 +45,17 @@ export const createUserMutation = `
 				name
 				email
 				avatarUrl
+				description
+				githubUrl
+				linkedinUrl
 				id
 			}
 		}
 	}
 `;
 
-export const createProjectMutation = `
-mutation CreateProject($input: ProjectCreateInput!) {
-  projectCreate(input: $input) {
-    project {
-      id
-      title
-      description
-      linkedinUrl
-	  githubUrl
-	  behanceUrl
-      createdBy {
-        email
-        name
-        avatarUrl
-      }
-    }
-  }
-}`;
-
-export const updateProfileMutation = `
-  mutation UpdateUser($id: ID!, $input: UserUpdateInput!) {
-    userUpdate(by: { id: $id }, input: $input) {
-      user {
-        id
-        name
-        avatarUrl
-        description
-        userGithubUrl
-        userBehanceUrl
-        userLinkedinUrl
-        userLiveSiteUrl
-      }
-    }
-  }
-`;
-
-export const updateProjectMutation = `
-	mutation UpdateProject($id: ID!, $input: ProjectUpdateInput!) {
-		projectUpdate(by: { id: $id }, input: $input) {
-		  project {
-			id
-			title
-			description
-			githubUrl
-			linkedinUrl
-			liveSiteUrl
-			behanceUrl
-			createdBy {
-			  email
-			  name
-			  avatarUrl
-			}
-		  }
-		}
-	}
-  
-`;
-
-export const deleteProjectMutation = `
-	mutation DeleteProject($id: ID!) {
-		projectDelete(by: { id: $id }) {
-			deletedId
-		}
-	}
-`;
-
 export const projectsQuery = `
-query ProjectCollection($endcursor: String, $category: String) {
+  query getProjects($category: String, $endcursor: String) {
     projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
       pageInfo {
         hasNextPage
@@ -110,8 +67,6 @@ query ProjectCollection($endcursor: String, $category: String) {
         node {
           title
           githubUrl
-          linkedinUrl
-          behanceUrl
           description
           liveSiteUrl
           id
@@ -130,46 +85,58 @@ query ProjectCollection($endcursor: String, $category: String) {
 `;
 
 export const getProjectByIdQuery = `
-query GetProjectById($id: ID!) {
-	project(by: { id: $id }) {
-	  id
-	  title
-	  description
-	  image
-	  liveSiteUrl
-	  githubUrl
-	  linkedinUrl
-	  description
-	  behanceUrl
-	  category
-	  createdBy {
-		id
-		name
-		email
-		avatarUrl
-		
-	  }
-	}
+  query GetProjectById($id: ID!) {
+    project(by: { id: $id }) {
+      id
+      title
+      description
+      image
+      liveSiteUrl
+      githubUrl
+      category
+      createdBy {
+        id
+        name
+        email
+        avatarUrl
+      }
+    }
+  }
+`;
+
+export const getUserQuery = `
+  query GetUser($email: String!) {
+    user(by: { email: $email }) {
+      id
+      name
+      email
+      avatarUrl
+      description
+      githubUrl
+      linkedinUrl
+    }
   }
 `;
 
 export const getProjectsOfUserQuery = `
-	query getUserProjects($id: ID!, $last: Int = 4) {
-		user(by: { id: $id }) {
-			id
-			name
-			email
-			avatarUrl
-			projects(last: $last) {
-				edges {
-					node {
-						id
-						title
-						image
-						category
-					}
-				}
-			}
-		}
-	}
+  query getUserProjects($id: ID!, $last: Int = 4) {
+    user(by: { id: $id }) {
+      id
+      name
+      email
+      description
+      avatarUrl
+      githubUrl
+      linkedinUrl
+      projects(last: $last) {
+        edges {
+          node {
+            id
+            title
+            image
+          }
+        }
+      }
+    }
+  }
 `;
